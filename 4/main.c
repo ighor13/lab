e7d773721743c3e1ca18eebfbcdb5941bb3f1eb0
ighor13 <ighor@ighor.ru>
2024-02-08 14:47:21 +0300

    Fix 14

--- 4/main.c
+++ 4/main.c
@@ -6,34 +6,27 @@ void clrscr();
 
 int main(void)
 {
+
     int *a, b;
     float *x, y = 3.5;
     double *m, n;
-    initscr();
     printf(" Enter b = ");
     scanf("%d", &b);
-    a = &b;
-    x = &y;
+    a = &b;								//ok
+    x = &y;								//ok
     printf(" a = %p\tb = %d\n", a, b);
-    getch();
-    b -= y;
+    b -= y;								// int=int-float ? ok но потеря точности
     printf(" a = %p\t*a = %d\tb = %d\n", a, *a, b);
-    getch();
-    m = a;
+    m = a;								// (double*) будет показывать на (int*) - в переменной непонятно что (значение, размеры значения)
     printf(" a = %p\tm = %p\n", a, m);
-    getch();
-    m = &n;
+    m = &n;								// ok
     printf(" n = %p\tm = %p\n", &n, m);
-    getch();
-    n = 5.5;
-    m++;
+    n = 5.5;								// ok
+    m++;								// указатель показывает на непонятный кусок памяти m+1=&n+1
     printf(" n = %f\tn = %p\tm = %p\n", n, &n, m);
-    getch();
-    *m = (float) *a - n + (int) *x;
+    *m = (float) *a - n + (int) *x;					// а теперь запись по непонятному адресу m+1 значения типа (float - double + int) -> double
     printf(" m = %p\t*m = %f\n", m, *m);
-    getch();
-    m -= 1;
+    m -= 1;								// m показывает обратно на n, а мы берем значение по адресу (m+1) - непонятно что записанное непонятно куда в предыдущей операции
     printf(" n = %f n = %p m = %p *(m+1) = %f\n", n, &n, m, *(m+1));
-    getch();
     return 0;
 }
